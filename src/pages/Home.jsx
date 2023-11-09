b5aaf5e2be3c6933cde877cee41b019fb7b92991
germanrogu <german.rodriguezgu@gmail.com>
2023-11-08 21:26:43 -0500

    Add Navbar component to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
