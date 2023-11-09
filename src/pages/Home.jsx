65f0efcd0b8317e2b357cba2de3e7c3df7fd2e77
germanrogu <german.rodriguezgu@gmail.com>
2023-11-08 23:34:15 -0500

    Remove Navbar component and handleSearch function from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,5 @@
 import React from "react";
-import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
-  const handleSearch = (searchQuery) => {
-    console.log("Realizar búsqueda con:", searchQuery);
-  };
-  return (
-    <div>
-      <Navbar onSearch={handleSearch} />
-    </div>
-  );
+  return <div></div>;
 };
