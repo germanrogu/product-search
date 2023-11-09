0458e0ec7532a5f8b88ea79f42e67f7c21a3cbed
germanrogu <german.rodriguezgu@gmail.com>
2023-11-08 23:17:01 -0500

    Add Navbar and 404 page to Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import { Home } from "../pages/Home";
 import { ProductDetail } from "../pages/ProductDetail";
 import { SearchResults } from "../pages/SearchResults";
+import { Navbar } from "../components/Navbar";
 
 const AppRouter = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='items' element={<SearchResults />}>
           <Route path=':id' element={<ProductDetail />} />
         </Route>
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
+
+  function NotFound() {
+    return <Navigate to='/' />;
+  }
 };
 
 export default AppRouter;
