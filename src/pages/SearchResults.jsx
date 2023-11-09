05ba002841ca4bfce35e19125cee38504e5c4b2e
germanrogu <german.rodriguezgu@gmail.com>
2023-11-09 11:07:28 -0500

    Add link to item details in search results and style changes

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../styles/SearchResults.scss";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 export const SearchResults = () => {
   const results = useSelector((state) => state.search.searchResults);
@@ -8,18 +9,30 @@ export const SearchResults = () => {
   return (
     <div className='search-results'>
       <div className='products-container'>
-        {results.map((item) => (
-          <div key={item.id} className='search-results-item'>
-            <img src={item.picture} alt={item.title} className='item-picture' />
-            <div className='item-details'>
-              <div className='item-price'>
-                {item.price.amount} {item.price.currency}
+        {results.length !== 0 ? (
+          results.map((item) => (
+            <Link
+              className='search-results-item'
+              key={item.id}
+              to={`/items/${item.id}`}
+            >
+              <img
+                src={item.picture}
+                alt={item.title}
+                className='item-picture'
+              />
+              <div className='item-details'>
+                <div className='item-price'>
+                  {item.price.amount} {item.price.currency}
+                </div>
+                <div className='item-title'>{item.title}</div>
+                <div className='item-condition'>{item.condition}</div>
               </div>
-              <div className='item-title'>{item.title}</div>
-              <div className='item-condition'>{item.condition}</div>
-            </div>
-          </div>
-        ))}
+            </Link>
+          ))
+        ) : (
+          <p>Cargando resultados...</p>
+        )}
       </div>
     </div>
   );
