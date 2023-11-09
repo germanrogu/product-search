46512c6b69b1523b4aaa9559326d832dcfbca77b
germanrogu <german.rodriguezgu@gmail.com>
2023-11-08 23:34:25 -0500

    Add search results display with styles

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import "../styles/SearchResults.scss";
+import { useSelector } from "react-redux";
 
 export const SearchResults = () => {
-  return <div>SearchResults</div>;
+  const results = useSelector((state) => state.search.searchResults);
+
+  return (
+    <div className='search-results'>
+      <div className='products-container'>
+        {results.map((item) => (
+          <div key={item.id} className='search-results-item'>
+            <img src={item.picture} alt={item.title} className='item-picture' />
+            <div className='item-details'>
+              <div className='item-price'>
+                {item.price.amount} {item.price.currency}
+              </div>
+              <div className='item-title'>{item.title}</div>
+              <div className='item-condition'>{item.condition}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
